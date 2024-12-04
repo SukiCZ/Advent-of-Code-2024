f36efbca79fb1de5d0e4cd0feda479d35346a4be
Jakub Boukal <www.bagr@gmail.com>
2024-12-04 23:27:33 +0100

    Change input_file variable to INPUT_FILE

--- day/01/python_01.py
+++ day/01/python_01.py
@@ -1,8 +1,11 @@
 from collections import defaultdict
 
 
-def read_lists(input_file: str) -> (list, list):
-    with open(input_file, "r") as file:
+INPUT_FILE = "input.txt"
+
+
+def read_lists() -> (list, list):
+    with open(INPUT_FILE, "r") as file:
         list_1, list_2 = [], []
         # Read file to list variables
         for line in file:
@@ -12,34 +15,33 @@ def read_lists(input_file: str) -> (list, list):
         return list_1, list_2
 
 
-def main(input_file: str):
+def main():
     result = 0
     # Read file to list variables
-    list_1, list_2 = read_lists(input_file)
+    list_1, list_2 = read_lists()
     # Sort the lists
     list_1.sort()
     list_2.sort()
     # Calculate the result
     for one, two in zip(list_1, list_2):
         result += abs(one - two)
-    print(f"The result is: {result}")
+    print(f"The result is: {result}")  # 2176849
 
 
-def part_two(input_file: str):
+def part_two():
     result = 0
     location_counter = defaultdict(int)
     # Read file to list variables
-    list_1, list_2 = read_lists(input_file)
+    list_1, list_2 = read_lists()
     # Count the number of times a value appears in the list_2
     for value in list_2:
         location_counter[value] += 1
     # Calculate the result
     for value in list_1:
         result += value * location_counter[value]
-    print(f"The part two result is: {result}")
+    print(f"The part two result is: {result}")  # 23384288
 
 
 if __name__ == "__main__":
-    input_file = "../input.txt"
-    main(input_file)
-    part_two(input_file)
+    main()
+    part_two()
