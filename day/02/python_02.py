d730c4d1e6dadc451d222928e6df254321754ec7
Jakub Boukal <www.bagr@gmail.com>
2024-12-04 23:59:31 +0100

    Day 02: Remove loop in main function

--- day/02/python_02.py
+++ day/02/python_02.py
@@ -1,7 +1,7 @@
 INPUT_FILE = "input.txt"
 
 
-def get_line():
+def read_lines():
     with open(INPUT_FILE, "r") as file:
         for line in file:
             yield line
@@ -29,15 +29,13 @@ def is_increasing_or_decreasing(reports: [int], allowed_fails: int = 0) -> bool:
                 return False
     return True
 
+
 def main():
-    safe_reports = 0
-    safe_reports_with_one_fail = 0
-    for line in get_line():
-        reports = [int(report) for report in line.split()]
-        if is_increasing_or_decreasing(reports):
-            safe_reports += 1
-        if is_increasing_or_decreasing(reports, allowed_fails=1):
-            safe_reports_with_one_fail += 1
+    lines = [[int(i) for i in line.split()] for line in read_lines()]
+    safe_reports = sum(is_increasing_or_decreasing(reports) for reports in lines)
+    safe_reports_with_one_fail = sum(
+        is_increasing_or_decreasing(reports, allowed_fails=1) for reports in lines
+    )
 
     print(f"Safe reports: {safe_reports}")  # 534
     print(f"Safe reports with allowed fail: {safe_reports_with_one_fail}")  # 577
