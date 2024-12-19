4b4acb0ea0a52fdb657a675a810c71fa57d01699
Jakub Boukal <www.bagr@gmail.com>
2024-12-19 09:34:19 +0100

    Add docstring to day 06 part 01

--- day/06/python_06.py
+++ day/06/python_06.py
@@ -13,55 +13,117 @@ DIRECTIONS_STR = [UP, RIGHT, DOWN, LEFT]
 
 
 class Guard:
-    def __init__(self, x, y):
+    """
+    Guard class represents the guard in the world.
+    """
+
+    def __init__(self, x: int, y: int):
+        """
+        Constructor for the Guard class.
+        :param x: X coordinate
+        :param y: Y coordinate
+        """
         self.x = x
         self.y = y
         self.direction = 0
 
     def move(self):
+        """
+        Move the guard in the direction it is facing.
+        """
         x, y = DIRECTIONS[self.direction]
         self.x += x
         self.y += y
 
     def turn(self):
+        """
+        Turn the guard 90° to the right.
+        """
         self.direction = (self.direction + 1) % 4
 
-    def position_ahead(self):
+    def position_ahead(self) -> tuple[int, int]:
+        """
+        Get the position ahead of the guard.
+        """
         x, y = DIRECTIONS[self.direction]
         return self.x + x, self.y + y
 
     def __str__(self):
+        """
+        String representation of the guard and its direction.
+        """
         return DIRECTIONS_STR[self.direction]
 
 
 class Obstacle:
+    """
+    Obstacle class represents an obstacle in the world.
+    """
+
     def __init__(self, x, y):
+        """
+        Constructor for the Obstacle class.
+        :param x: X coordinate
+        :param y: Y coordinate
+        """
         self.x = x
         self.y = y
 
     def __str__(self):
+        """
+        String representation of the obstacle.
+        """
         return OBSTACLE
 
 
 class World:
-    def __init__(self, data):
-        self.data = data
-        self.start = (0, 0)
-        self.guard = None
-        self.obstacles = set()
-        self.visited = set()
-        self.height = len(data)
-        self.width = len(data[0])
+    """
+    World-class represents the world the guard is walking in.
+    """
+
+    def __init__(self, data: list[str]):
+        """
+        Constructor for the World class.
+        :param data: List of strings representing the world.
+        """
+        self.height: int = len(data)
+        self.width: int = len(data[0])
+        self.guard: Guard | None = None
+        self.obstacles: set = set()
+        self.visited: set = set()
 
         for i in range(self.height):
             for j in range(self.width):
                 if data[i][j] == UP:
-                    self.start = (i, j)
                     self.guard = Guard(i, j)
                 elif data[i][j] == OBSTACLE:
                     self.obstacles.add(Obstacle(i, j))
 
+    def walk(self):
+        """
+        Walk the guard in the world.
+        """
+        x, y = self.guard.position_ahead()
+
+        if any([o.x == x and o.y == y for o in self.obstacles]):
+            self.guard.turn()
+            return
+
+        visit = (self.guard.x, self.guard.y)
+        if visit not in self.visited:
+            self.visited.add(visit)
+        self.guard.move()
+
+    def on_map(self, x, y) -> bool:
+        """
+        Check if the given coordinates are on the map.
+        """
+        return 0 <= x < self.height and 0 <= y < self.width
+
     def print(self):
+        """
+        Print the world.
+        """
         for i in range(self.height):
             for j in range(self.width):
                 if self.guard.x == i and self.guard.y == j:
@@ -75,22 +137,10 @@ class World:
             print("")
         print("\n\n\n")
 
-    def walk(self):
-        x, y = self.guard.position_ahead()
-
-        if any([o.x == x and o.y == y for o in self.obstacles]):
-            self.guard.turn()
-            return
-
-        visit = (self.guard.x, self.guard.y)
-        if visit not in self.visited:
-            self.visited.add(visit)
-        self.guard.move()
-
-    def on_map(self, x, y):
-        return 0 <= x < self.height and 0 <= y < self.width
-
     def print_stats(self):
+        """
+        Print the statistics of the world.
+        """
         print(
             {
                 "height": self.height,
@@ -111,7 +161,7 @@ def main():
         if not world.on_map(world.guard.x, world.guard.y):
             break
 
-    world.print_stats()
+    world.print_stats()  # visited: 4647, obstacles: 807
     world.print()
 
 
