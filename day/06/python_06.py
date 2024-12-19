414ca264bc66f429fa523b3a3a67b23da3fdfab5
Jakub Boukal <www.bagr@gmail.com>
2024-12-19 11:39:13 +0100

    Add day 06 part 02

--- day/06/python_06.py
+++ day/06/python_06.py
@@ -1,4 +1,5 @@
 INPUT = "input.txt"
+# INPUT = "input_small.txt"
 
 UP = "^"
 RIGHT = ">"
@@ -89,6 +90,7 @@ class World:
         self.height: int = len(data)
         self.width: int = len(data[0])
         self.guard: Guard | None = None
+        self.start: tuple[int, int] = (0, 0)
         self.obstacles: set = set()
         self.visited: set = set()
 
@@ -96,10 +98,11 @@ class World:
             for j in range(self.width):
                 if data[i][j] == UP:
                     self.guard = Guard(i, j)
+                    self.start = (i, j)
                 elif data[i][j] == OBSTACLE:
                     self.obstacles.add(Obstacle(i, j))
 
-    def walk(self):
+    def walk(self, add_visit: bool = True):
         """
         Walk the guard in the world.
         """
@@ -109,9 +112,10 @@ class World:
             self.guard.turn()
             return
 
-        visit = (self.guard.x, self.guard.y)
-        if visit not in self.visited:
-            self.visited.add(visit)
+        if add_visit:
+            visit = (self.guard.x, self.guard.y)
+            if visit not in self.visited:
+                self.visited.add(visit)
         self.guard.move()
 
     def on_map(self, x, y) -> bool:
@@ -119,6 +123,51 @@ class World:
         Check if the given coordinates are on the map.
         """
         return 0 <= x < self.height and 0 <= y < self.width
+
+    def check_loop(self, obstacle: tuple[int, int]) -> bool:
+        """
+        Check if there is a loop in the world.
+        :param obstacle: Obstacle to be added to create a time paradox.
+        :return: True if there is a loop, False otherwise.
+        """
+
+        # Reset guard position and direction
+        self.guard.direction = 0
+        self.guard.x, self.guard.y = self.start
+
+        # Locations and directions visited
+        states = set()
+
+        while self.on_map(self.guard.x, self.guard.y):
+            # Add visited state
+            states.add(((self.guard.x, self.guard.y), self.guard.direction))
+            # Get position ahead
+
+            # Turn if there is an obstacle or the position ahead is the obstacle
+            while Obstacle(*self.guard.position_ahead()) in self.obstacles or self.guard.position_ahead() == obstacle:
+                self.guard.turn()
+
+            # If the position ahead and the direction is in the states, there is a loop
+            if (self.guard.position_ahead(), self.guard.direction) in states:
+                return True
+
+            # Walk without changing the visited set
+            self.walk(add_visit=False)
+
+        return False
+
+
+    def check_obstacles(self) -> int:
+        """
+        Check if there are obstacles to add to create a time paradox.
+        :return: Number of obstacles to add.
+        """
+        obstacles_to_add = 0
+        for obstacle in self.visited:
+            if self.check_loop(obstacle):
+                obstacles_to_add += 1
+                print(".", end="")  # This takes a while
+        return obstacles_to_add
 
     def print(self):
         """
@@ -157,12 +206,16 @@ def main():
 
     world = World(data)
     while True:
+        # world.print()
         world.walk()
         if not world.on_map(world.guard.x, world.guard.y):
             break
 
-    world.print_stats()  # visited: 4647, obstacles: 807
     world.print()
+    world.print_stats()  # visited: 4647, obstacles: 807
+
+    check_obstacles = world.check_obstacles()
+    print(f"Obstacles to add: {check_obstacles}")  # 1723
 
 
 if __name__ == "__main__":
