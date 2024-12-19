19cd5bfada6dc7d7b6765b60588df2aa8ebf6f88
Jakub Boukal <www.bagr@gmail.com>
2024-12-19 16:00:09 +0100

    Add day 07 part 02

--- day/07/python_07.py
+++ day/07/python_07.py
@@ -22,17 +22,21 @@ def get_input() -> tuple[int, list[int]]:
             yield test_value, numbers
 
 
-def can_solve(test_value, result, numbers):
+def can_solve(test_value: int, result: int, numbers: list[int], calibrate=False):
     """
     Solve equations using addition (`+`) and multiplication (`*`).
+
+    :param: calibrate: If True, it will allow to concatenate numbers. Default is False.
 
     Example:
     190: 10 19
     3267: 81 40 27
+    7290: 6 8 6 15  # calibrate=True
 
     Result:
     190 = 10 * 19
     3267 = 81 * 40 + 27 (or 81 + 40 * 27)
+    7290 = 6 * 8 || 6 * 15
     """
     if result > test_value:
         return False
@@ -43,8 +47,9 @@ def can_solve(test_value, result, numbers):
     number = numbers.pop(0)
     return any(
         [
-            can_solve(test_value, result + number, numbers.copy()),
-            can_solve(test_value, result * number, numbers.copy()),
+            can_solve(test_value, result + number, numbers.copy(), calibrate),
+            can_solve(test_value, result * number, numbers.copy(), calibrate),
+            can_solve(test_value, int(f"{result}{number}"), numbers.copy(), calibrate) if calibrate else False
         ]
     )
 
@@ -55,6 +60,11 @@ def main():
         if can_solve(test_value, 0, numbers):
             test_values += test_value
     print(f"Sum of test values: {test_values}")  # 2664460013123
+    test_values_with_calibration = 0
+    for test_value, numbers in get_input():
+        if can_solve(test_value, 0, numbers, calibrate=True):
+            test_values_with_calibration += test_value
+    print(f"Sum of test values with calibration: {test_values_with_calibration}")  # 426214131924213
 
 
 if __name__ == "__main__":
