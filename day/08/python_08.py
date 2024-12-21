789147ad2d59da712f6fe6b54b09158d6387b969
Jakub Boukal <www.bagr@gmail.com>
2024-12-21 10:35:13 +0100

    Add day 08 part 02

--- day/08/python_08.py
+++ day/08/python_08.py
@@ -60,30 +60,47 @@ class World:
         """
         return 0 <= location.x < self.height and 0 <= location.y < self.width
 
-    def add_antinodes(self, location: Location, direction: Location):
+    def add_antinodes(
+        self, location: Location, direction: Location, start: int = 1, stop: int = 2
+    ):
         """
         Extend antinode positions in a direction (dx, dy)
+
+        :param location: Location of the antenna
+        :param direction: Direction to extend the antinodes
+        :param start: Start distance to extend the antinodes
+        :param stop: Stop distance to extend the antinodes
         """
-        ax, ay = location.x + direction.x, location.y + direction.y
-        if self.on_map(Location(ax, ay)):
-            # Antinodes can't overlap each other
-            if self.grid[ax][ay] != ANITONODE:
-                self.antinodes.add(Location(ax, ay))
-                if self.grid[ax][ay] == EMPTY:
-                    # Add to grid not to overlap with antennas
+        for i in range(start, stop):
+            ax, ay = location.x + (i * direction.x), location.y + (i * direction.y)
+            if self.on_map(Location(ax, ay)):
+                # Antinodes can't overlap each other
+                if self.grid[ax][ay] != ANITONODE:
+                    self.antinodes.add(Location(ax, ay))
                     self.grid[ax][ay] = "#"
 
-    def deploy_antinodes(self):
+    def deploy_antinodes(self, resonant_harmonics=False):
         """
         Deploy antinodes for all antennas
 
-        For combination of each antenna pair, deploy antinodes in the direction of antenna pair
+        For combination of each antenna pair, deploy antinodes in the direction of antenna pair.
+
+        :param: resonant_harmonics: If True, deploy antinodes in N distance of antenna pair direction
         """
+
+        max_size = max(self.height, self.width)
+        start = 0 if resonant_harmonics else 1
+        stop = max_size if resonant_harmonics else 2
+
         for antenna, locations in self.antennas.items():
             for loc_1, loc_2 in combinations(locations, 2):
                 dx, dy = loc_2.x - loc_1.x, loc_2.y - loc_1.y
-                self.add_antinodes(Location(loc_1.x, loc_1.y), Location(-dx, -dy))
-                self.add_antinodes(Location(loc_2.x, loc_2.y), Location(dx, dy))
+                self.add_antinodes(
+                    Location(loc_1.x, loc_1.y), Location(-dx, -dy), start, stop
+                )
+                self.add_antinodes(
+                    Location(loc_2.x, loc_2.y), Location(dx, dy), start, stop
+                )
 
     def print(self):
         """
@@ -97,10 +114,12 @@ class World:
 def main():
     data = read_input()
     world = World(data)
-    # world.print()
     world.deploy_antinodes()
     # world.print()
     print(f"Lenght of antinodes: {len(world.antinodes)}")  # 332
+    world.deploy_antinodes(resonant_harmonics=True)
+    # world.print()
+    print(f"Lenght of antinodes: {len(world.antinodes)}")  # 1174
 
 
 if __name__ == "__main__":
