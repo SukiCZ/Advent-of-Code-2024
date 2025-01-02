56fe348e10d71dd799f7f565b8ec264845dbd675
Jakub Boukal <www.bagr@gmail.com>
2025-01-02 23:34:26 +0100

    Add day 19 part 02

--- day/19/python_19.py
+++ day/19/python_19.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 INPUT = "input.txt"
 
 
@@ -28,10 +30,35 @@ def can_form_pattern(towels: set[str], pattern: str) -> bool:
     return False
 
 
+def count_formations(towels: set[str], patterns: list[str]) -> int:
+    """
+    Recursive function to count the number of formations of a pattern.
+    :param towels: Towels to use to form a pattern.
+    :param patterns: Patterns to form.
+    :return: Number of formations of the pattern.
+    """
+
+    @cache
+    def count_formation(pattern: str) -> int:
+        count = 0
+        for towel in towels:
+            # If the towel is the pattern, increment the count.
+            if towel == pattern:
+                count += 1
+            # If the pattern starts with the towel, count the number of formations of the rest of the pattern.
+            if pattern.startswith(towel):
+                count += count_formation(pattern[len(towel) :])
+        return count
+
+    return sum([count_formation(pattern) for pattern in patterns])
+
+
 def main():
     towels, patterns = read_input()
-    count = sum([can_form_pattern(towels, pattern) for pattern in patterns])
-    print(f"{count} patterns can be formed by the towels.")
+    count_can_form = sum([can_form_pattern(towels, pattern) for pattern in patterns])
+    count_formation = count_formations(towels, patterns)
+    print(f"{count_can_form} patterns can be formed by the towels.")
+    print(f"{count_formation} formations of the patterns can be made by the towels.")
 
 
 if __name__ == "__main__":
